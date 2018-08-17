ad48e4b8d98dc27e446aefbf0e8d348071be0b7b
cornerStone-Dev <cornerStone-dev@gmail.com>
2018-08-17 17:20:31 -0400

    Added detail about running out of memory

--- TestAppiOS/ViewController.swift
+++ TestAppiOS/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        // Memory is about to run out, prepare for crash
     }
 
 
